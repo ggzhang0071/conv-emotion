674a76ce5808dd03cac7f50eabb7b05a0b30ea7b
ggzhang0071 <155051843@qq.com>
2022-01-28 13:58:53 +0800

    tmp

---
+++ COSMIC/erc-training/docker_cmd_93.sh
@@ -0,0 +1,22 @@
+#docker_cmd_93.sh
+img="nvcr.io/nvidia/pytorch:19.01-py3"
+#nvcr.io/nvidia/pytorch:18.01-py3
+
+
+
+docker run --gpus all  --privileged=true   --workdir /git --name "cosmic"  -e DISPLAY --ipc=host -d --rm  -p 6610:4452  \
+-v /raid/git/conv-emotion:/git/conv-emotion \
+ -v /raid/git/datasets:/git/datasets \
+ $img sleep infinity
+
+
+docker exec -it cosmic   /bin/bash
+
+cd  conv-emotion
+
+#pip install -r COSMIC/requirements.txt
+#docker images |grep pytorch  | grep  "19"
+
+#pip list  |grep "pytorch"
+
+#docker stop cosmic
