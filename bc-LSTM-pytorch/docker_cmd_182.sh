08f1d0a28cf84349701fe3a2d18277a355090acb
ggzhang0071 <155051843@qq.com>
2022-03-01 17:38:39 +0800

    code ok

---
+++ bc-LSTM-pytorch/docker_cmd_182.sh
@@ -0,0 +1,15 @@
+img="nvcr.io/nvidia/pytorch:19.01-py3"
+#nvcr.io/nvidia/pytorch:18.01-py3
+
+
+docker run --gpus all  --privileged=true   --workdir /git --name "bclstm"  -e DISPLAY --ipc=host -d --rm  -p 6610:4452  \
+-v /raid/git/conv-emotion/bc-LSTM-pytorch:/git/bc-LSTM-pytorch \
+ -v  /raid/git/datasets:/git/datasets \
+ $img sleep infinity
+
+
+docker exec -it bclstm   /bin/bash
+
+
+#pip install -r COSMIC/requirements.txt
+#docker images |grep pytorch  | grep  "19"
