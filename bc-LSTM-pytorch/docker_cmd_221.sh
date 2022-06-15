23ad343c7ac29c6209bc958d9274f559cc19c3b1
Victor Li <vili@nvidia.com>
2022-06-15 14:03:01 +0800

    code ok

---
+++ bc-LSTM-pytorch/docker_cmd_221.sh
@@ -0,0 +1,17 @@
+#img="nvcr.io/nvidia/pytorch:19.01-py3"
+img="nvcr.io/nvidia/pytorch:20.12-py3"
+
+#nvcr.io/nvidia/pytorch:18.01-py3
+
+
+docker run --gpus all  --privileged=true   --workdir /git --name "bclstm"  -e DISPLAY --ipc=host -d --rm  -p 6620:4452  \
+-v /mnt/work/git/conv-emotion/bc-LSTM-pytorch:/git/bc-LSTM-pytorch \
+ -v  /mnt/work/git/datasets:/git/datasets \
+ $img sleep infinity
+
+docker exec -it bclstm   /bin/bash
+
+#pip install -r COSMIC/requirements.txt
+#docker images |grep pytorch  | grep  "19"
+
+#docker stop bclstm
